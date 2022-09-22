7dad0ed20747301213889d387003e978c31e82d0
Florencia <muruzagasti87@gmail.com>
2022-09-22 02:33:08 -0300

    correcciones en lista

--- ejercicio.js
+++ ejercicio.js
@@ -1,10 +1,12 @@
-"Maria Florencia"
-26
-false;
-const ahora = new Date
-const libroFav = {
+const lista =[
+    "Maria Florencia",
+    26,
+    false,
+    new Date(1995, 10, 27),
+    libroFav = {
     titulo:"Shingeki no Kyojin",
     autor: "Hashime Isayama",
-    fecha: 2010,
+    fecha: new Date(2010, 2, 1),
     url: "https://snk.manganyaa.com/leer-online-gratis-espanol/capitulo/1"
-}
+    }
+]
