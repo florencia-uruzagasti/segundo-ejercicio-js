a11bc7508049bd7b7bbee60d3abc798e7e1af0f5
Florencia <muruzagasti87@gmail.com>
2022-09-22 02:27:57 -0300

    ejercicio js

---
+++ ejercicio.js
@@ -0,0 +1,10 @@
+"Maria Florencia"
+26
+false;
+const ahora = new Date
+const libroFav = {
+    titulo:"Shingeki no Kyojin",
+    autor: "Hashime Isayama",
+    fecha: 2010,
+    url: "https://snk.manganyaa.com/leer-online-gratis-espanol/capitulo/1"
+}
